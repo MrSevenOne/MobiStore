47bbeb005fbe5f85487c1b9d3e1ffecb6610d63d
macbook <mirvoxidraxmonbekov@gmail.com>
2025-08-08 17:17:13 +0500

    add localization

---
+++ lib/config/localization/ru_ru.dart
@@ -0,0 +1,16 @@
+const Map<String, String> ruRU = {
+  // SIGN IN PAGE
+  'title': 'Снова здравствуйте',
+  'subtitle': 'С возвращением, мы скучали по вам',
+  'email': 'Электронная почта',
+  'email_hint': 'Введите email',
+  'email_empty': 'Пожалуйста, введите email',
+  'email_invalid': 'Неверный адрес электронной почты',
+  'password': 'Пароль',
+  'password_hint': 'Введите пароль',
+  'password_empty': 'Пожалуйста, введите пароль',
+  'recovery_password': 'Восстановить пароль',
+  'sign_in': 'Войти',
+  'dont_have_account': 'Нет аккаунта?',
+  'sign_up': 'Зарегистрироваться'
+};
---
+++ lib/config/localization/uz_uz.dart
@@ -0,0 +1,17 @@
+const Map<String, String> uzUZ = {
+  // SIGN IN PAGE
+  'title': 'Hello, Again',
+  'subtitle': 'Xush kelibsiz, siz sog\'indik',
+  'email': 'Email manzil',
+  'email_hint': 'Email kiriting',
+  'email_empty': 'Iltimos, email kiriting',
+  'email_invalid': 'Email noto\'g\'ri',
+  'password': 'Parol',
+  'password_hint': 'Parol kiriting',
+  'password_empty': 'Iltimos, parol kiriting',
+  'recovery_password': 'Parolni tiklash',
+  'sign_in': 'Tizimga kirish',
+  'dont_have_account': 'Akkountingiz yo\'qmi?',
+  'sign_up': 'Ro\'yxatdan o\'tish',
+
+};
