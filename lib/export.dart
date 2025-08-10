57820d7431dc7cf531a6bc23c35e87b338bec5d7
macbook <mirvoxidraxmonbekov@gmail.com>
2025-08-10 23:28:43 +0500

    change code

--- lib/export.dart
+++ lib/export.dart
@@ -1,6 +1,15 @@
 //material
 export 'package:flutter/material.dart';
+//AppRout
+export 'package:mobi_store/routing/app_router.dart';
 
+
+//Config
+export 'package:mobi_store/config/constants/ui_constants.dart';
+
+
+//Widgets
+export 'package:mobi_store/ui/tariff/widgets/tariff_card.dart';
 
 // Constants
 export 'package:mobi_store/config/constants/supabase_constants.dart';
@@ -8,4 +17,45 @@ export 'package:mobi_store/config/constants/supabase_constants.dart';
 
 //DataBase
 export 'package:supabase_flutter/supabase_flutter.dart';
+
+
+
+//Provider
+export 'package:mobi_store/ui/auth/view_model/auth_view_model.dart';
+export 'package:mobi_store/ui/tariff/view_model/tariff_view_model.dart';
+export 'package:mobi_store/ui/provider/usertariff_viewmodel.dart';
+
+
+
+
+
+//Service
+export 'package:mobi_store/data/services/auth/auth_service.dart';
+export 'package:mobi_store/data/services/base_service.dart';
+export 'package:mobi_store/data/services/data/user_service.dart';
+export 'package:mobi_store/data/services/data/tariffs_service.dart';
+export 'package:mobi_store/data/services/data/usertariff_service.dart';
+
+
+
+
+//Utils
+export 'package:mobi_store/utils/user/user_manager.dart';
+export 'package:get/get_utils/get_utils.dart';
+
+//Models
+export 'package:mobi_store/domain/models/user_model.dart';
+export 'package:mobi_store/domain/models/tariffs_model.dart';
+export 'package:mobi_store/domain/models/usertariff_model.dart';
+
+
+
+
+
+
+//Packages
+export 'package:google_fonts/google_fonts.dart';
+export 'package:provider/provider.dart';
+export 'package:carousel_slider/carousel_slider.dart';
+
 
