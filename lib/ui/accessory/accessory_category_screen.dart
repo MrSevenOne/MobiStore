1364bdd47ea2537233cffb1fd9ed313567f240fa
macbook <mirvoxidraxmonbekov@gmail.com>
2025-08-25 18:50:49 +0500

    Ui change FutureBuilder to Consumer

--- lib/ui/accessory/accessory_category_screen.dart
+++ lib/ui/accessory/accessory_category_screen.dart
@@ -8,73 +8,73 @@ class AccessoryCategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel =
-        Provider.of<AccessoryCategoryViewModel>(context, listen: false);
     final theme = Theme.of(context);
 
     return Scaffold(
       appBar: CustomAppBar(),
       drawer: CustomDrawer(),
-      body: FutureBuilder(
-        future: viewModel.fetchCategories(), // ma’lumotni olish
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-                child: CircularProgressIndicator()); // yuklanmoqda
-          } else if (snapshot.hasError) {
-            return Center(child: Text("Xatolik: ${snapshot.error}"));
-          } else {
-            return Consumer<AccessoryCategoryViewModel>(
-              builder: (context, viewmodel, child) {
-                if (viewmodel.categories.isEmpty) {
-                  return const Center(child: Text("Kategoriyalar topilmadi"));
-                }
-                return ListView.builder(
-                  itemCount: viewmodel.categories.length,
-                  itemBuilder: (context, index) {
-                    final category = viewmodel.categories[index];
-                    return Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 6.0,
-                        horizontal: UiConstants.padding,
-                      ),
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: theme.colorScheme.secondary,
-                        borderRadius:
-                            BorderRadius.circular(UiConstants.borderRadius),
-                      ),
-                      child: InkWell(
-                        onTap: () => Navigator.pushNamed(
-                          context,
-                          AppRouter.accessories,
-                          arguments: category,
-                        ),
-                        child: Row(
-                          spacing: 12.0,
-                          children: [
-                            Image.asset(
-                              'assets/icons/category.png',
-                              height: 28,
-                            ),
-                            Text(
-                              category.name,
-                              style: theme.textTheme.bodyLarge,
-                            ),
-                            Spacer(),
-                            Text(
-                              "(${viewmodel.categoryCounts[category.id] ?? 0})",
-                              style: theme.textTheme.bodySmall,
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-            );
+      body: Consumer<AccessoryCategoryViewModel>(
+        builder: (context, viewModel, child) {
+          // Loading indicator
+          if (viewModel.isLoading) {
+            return const Center(child: CircularProgressIndicator());
           }
+
+          // Error ko‘rsatish
+          if (viewModel.errorMessage != null) {
+            return Center(child: Text(viewModel.errorMessage!));
+          }
+
+          // Hech qanday category yo‘q
+          if (viewModel.categories.isEmpty) {
+            return const Center(child: Text("Kategoriyalar topilmadi"));
+          }
+
+          // List
+          return ListView.builder(
+            itemCount: viewModel.categories.length,
+            itemBuilder: (context, index) {
+              final category = viewModel.categories[index];
+              final count = viewModel.categoryCounts[category.id] ?? 0;
+
+              return Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 6.0,
+                  horizontal: UiConstants.padding,
+                ),
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(UiConstants.borderRadius),
+                ),
+                child: InkWell(
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    AppRouter.accessories,
+                    arguments: category,
+                  ),
+                  child: Row(
+                    spacing: 12.0,
+                    children: [
+                      Image.asset(
+                        'assets/icons/category.png',
+                        height: 28,
+                      ),
+                      Text(
+                        category.name,
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                      const Spacer(),
+                      Text(
+                        "($count)",
+                        style: theme.textTheme.bodySmall,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
         },
       ),
     );
